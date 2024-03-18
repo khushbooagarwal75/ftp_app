00694ea55a94f97a7190d97c2aaa7db06644ccbf
Khushboo Agarwal <khushbooagarwal7777@gmail.com>
2024-03-18 16:30:38 +0530

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:ftp_app/Add.dart';
+
 import 'package:ftp_app/database/database_service.dart';
 import 'package:ftp_app/database/ftp_db.dart';
+import 'package:ftp_app/try.dart';
 import 'package:get/get.dart';
 
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
 }
 
 class Login extends StatefulWidget {
-  Login({super.key});
+  const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
@@ -40,6 +41,7 @@ class _LoginState extends State<Login> {
   TextEditingController password_tc = TextEditingController();
 
   bool visible = false;
+  bool tapped=false;
   late DatabaseService databaseService;
   @override
   void initState() {
@@ -78,126 +80,207 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Finished Part Transfer"),
-        centerTitle: true,
-        backgroundColor: Colors.amber,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextFormField(
-                      controller: companyid,
-                      decoration: InputDecoration(
-                          hintText: "Company Id",
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 2,
-                              ))),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter  CompanyId';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      controller: username,
-                      decoration: InputDecoration(
-                          hintText: "Username",
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 2,
-                              ))),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter  Username';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      controller: password_tc,
-                      decoration: InputDecoration(
-                          hintText: "Password",
-                          suffixIcon: IconButton(
-                            icon: Icon(visible
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                            onPressed: () {
-                              setState(() {
-                                visible = !visible;
-                              });
-                            },
-                          ),
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 2,
-                              ))),
-                      obscureText: visible,
-                      obscuringCharacter: "*",
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter  password';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              final user = username.text;
-                              final password = password_tc.text;
-                              final id = int.parse(companyid.text);
-                              var uservalid = await FtpDB().getUser(id, user, password);
-
-                                            // Check if user is not null to determine if credentials are valid
-                                            if (uservalid != null) {
-                                              Get.to(() => Showinformation(), arguments:{
-                                                'username':user,
-                                              });
-                                            } else {
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                SnackBar(
-                                                  content: Text('Invalid Credentials'),
-                                                ),
-                                              );
-                              }
-                              } else {
-                                // User doesn't exist, handle accordingly
-                                print('User does not exist');
-                              }
-                            },
-                          child: Text("Log In"),
-
-                    ),
-                    ),
-                  ],
-                ),
+        backgroundColor: Colors.white ,
+        body: Stack(
+          children: [
+            ClipPath(
+              child: Container(
+                color:Colors.cyan.shade600,
+                // Color.fromRGBO(200,250,156,1),
               ),
-            ],
-          ),
-        ),
-      ),
+              clipper: custom(),
+            ),
+            Positioned(
+              width: 410,
+              top:  MediaQuery.of(context).size.height/12,
+              child: Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color:Color.fromRGBO(130, 110, 224, 1.0),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: FlutterLogo(
+                      style: FlutterLogoStyle.markOnly,
+                    ),
+                  ),
+                  SizedBox(height: 100,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 500,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Text("LOGIN",style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              TextFormField(
+                                controller: companyid,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black87,width: 4),
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here
+                                  ),
+                                  hintText: "Company Id",
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here as well
+                                  ),),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter  CompanyId';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              TextFormField(
+                                controller: username,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black87,width: 4),
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here
+                                  ),
+                                  hintText: "Username in email format",
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here as well
+                                  ),),
+                                keyboardType: TextInputType.emailAddress,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter  Username';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                controller: password_tc,
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  suffixIcon: IconButton(
+                                    icon: Icon(visible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off),
+                                    onPressed: () {
+                                      setState(() {
+                                        visible = !visible;
+                                      });
+                                    },
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black87,width: 4),
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)// Change the border color here as well
+                                  ),),
+                                obscuringCharacter: "*",
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter  password';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.black,
+                                      backgroundColor: Colors.cyan.shade600,
+                                      textStyle: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.bold,
+
+                                      )
+                                    // Set the text color to white
+                                  ),
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(() {
+                                        tapped=true;
+                                      });
+                                      final user = username.text;
+                                      final password = password_tc.text;
+                                      final id = int.parse(companyid.text);
+                                      var uservalid = await FtpDB().getUser(id, user, password);
+
+                                      // Check if user is not null to determine if credentials are valid
+                                      if (uservalid != null) {
+                                        Get.to(() => Bar(), arguments:{
+                                          'username':user,
+                                        });
+                                      } else {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(
+                                            content: Text('Invalid Credentials'),
+                                          ),
+                                        );
+                                      }
+                                    } else {
+                                      // User doesn't exist, handle accordingly
+                                      print('User does not exist');
+                                    }
+                                  },
+                                  child: tapped? CircularProgressIndicator():Text("Login"),
+
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
     );
   }
+}
+class custom extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    var path= new Path();
+    path.lineTo(0.0, size.height/2.5);
+    path.lineTo(size.width-100, 0.0);
+
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+
 }
